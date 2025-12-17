61ef52921bc548a5f771f532bf356c083c0cea72
Wilson <wilsoncheong7405@gmail.com>
2025-12-16 22:48:21 -0500

    row count and schema

--- pymongo_get_database.py
+++ pymongo_get_database.py
@@ -1,23 +1,32 @@
 import os
 from pymongo import MongoClient
 import pandas as pd
+import logging 
 from dotenv import load_dotenv
 
 load_dotenv()
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger(__name__)
 
 MONGO_URI = os.getenv("MONGO_URI")
 
 client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
 
 client.admin.command("ping")
-print("Connected to MongoDB Atlas")
+logging.info("Connected to MongoDB Atlas")
 
+# access database 
 db = client["bigdata_capstone"]
+# access table
 col = db["stocks_raw"]
 
-print("Document count:", col.estimated_document_count())
-
-df = pd.DataFrame(list(col.find().limit(5)))
+df = pd.DataFrame(col.find())
 print(df)
+logging.info("dataframe table done")
+
+table_count = df.shape[0]
+print(table_count)
+logging.info("row count and schema")
 
 
